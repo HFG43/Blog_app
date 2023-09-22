630a834ae07f7eb0ef2acaf74ce883331a0431f6
HFG43 <hernanguemes@hotmail.com>
2023-09-21 22:19:38 -0300

    Feat: add likes counter method

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,11 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post  
+
+  after_create :update_likes_counter
+  after_destroy :update_likes_counter
+
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
