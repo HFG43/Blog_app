515b5e31f4cfbfbd130a99afbaa23e5c5e5adbf5
HFG43 <hernanguemes@hotmail.com>
2023-09-22 13:48:07 -0300

    Feat: add post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,13 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true
+  validates :title, length: { maximun: 250 }
+
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_create :update_post_counter
   after_destroy :update_post_counter
 
