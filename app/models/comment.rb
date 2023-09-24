8fb82cc06be593818122f67dfc86acbba6a2b673
HFG43 <hernanguemes@hotmail.com>
2023-09-24 20:08:03 -0300

    Misc: improve FK in models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :user, class_name: 'User'
-  belongs_to :post, class_name: 'Post'
+  belongs_to :user, class_name: 'User', foreign_key: 'users_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'posts_id'
 
   after_create :update_comment_counter
   after_destroy :update_comment_counter
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :user, class_name: 'User'
-  belongs_to :post, class_name: 'Post'
+  belongs_to :user, class_name: 'User', foreign_key: 'users_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'posts_id'
 
   after_create :update_likes_counter
   after_destroy :update_likes_counter
