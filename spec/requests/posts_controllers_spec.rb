01b1e20d7460958cbe7864d0a8ed820911471828
HFG43 <hernanguemes@hotmail.com>
2023-09-27 21:47:58 -0300

    Feat: add placeholder in template specs

--- spec/requests/posts_controllers_spec.rb
+++ spec/requests/posts_controllers_spec.rb
@@ -1,20 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe "PostsControllers", type: :request do
+  before(:each) do
+    @user = User.create(name: "Hernan", bio: "Back end developer")
+    @post = Post.create(author: @user, title: 'Spec test', text: 'This is a request test')
+  end
+
   describe "GET /users/:user_id/posts" do
     it "Index path works fine" do
-      user_id = 1 
-      get user_posts_path(user_id)
+      get user_posts_path(@user.id)
       expect(response).to have_http_status(200)
+    end
+
+    it "renders Index path template" do
+      get user_posts_path(@user.id)
+      expect(response).to render_template(:index)
+    end
+
+    it "includes correct placeholder text in Index" do
+      get user_posts_path(@user)
+      expect(response.body).to include("Here you will see all the posts of a particular user")
     end
   end
 
   describe "GET /users/:user_id/posts/:id" do
     it "Show path works fine" do
-      user_id = 1
-      post_id = 2
-      get user_posts_path(user_id, post_id)
+      user_id = @user.id
+      post_id = @post.id
+      get user_post_path(user_id, post_id)
       expect(response).to have_http_status(200)
     end
+
+    it "renders Show path template" do
+      user_id = @user.id
+      post_id = @post.id
+      get user_path(user_id, post_id)
+      expect(response).to render_template(:show)
+    end
+
+    it "includes correct placeholder text in Show" do
+      get user_post_path(@user, @post)
+      expect(response.body).to include("You will find here all comments & likes of a particular post from a particular user")
+    end
   end
-end
+end  
--- spec/requests/users_controllers_spec.rb
+++ spec/requests/users_controllers_spec.rb
@@ -15,19 +15,27 @@ RSpec.describe "UsersControllers", type: :request do
       get users_path
       expect(response).to render_template(:index)
     end
+
+    it "includes correct placeholder text in Index" do
+      get users_path
+      expect(response.body).to include("Here you will soon see the list of all users")
+    end
   end
 
   describe "GET /users/:user_id" do
     it "Show path works fine" do
-      user_id = 1
       get user_path(@user)
       expect(response).to have_http_status(200)
     end
 
     it "renders Show path template" do
-      user_id = 1
       get user_path(@user)
       expect(response).to render_template(:show)
+    end
+
+    it "includes correct placeholder text in Show" do
+      get user_path(@user)
+      expect(response.body).to include("Here you will be able to see all data refered to an user")
     end
   end
 end
