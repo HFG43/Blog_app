e86ad5f11da10651ff727a71211d19077a52fcb8
HFG43 <hernanguemes@hotmail.com>
2023-09-28 21:59:00 -0300

    Misc: rubocop line carriage correction

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   def index
-    if params[:user_id]
-      @author = User.find(params[:user_id])
-      @posts = @author.posts
-    end  
+    return unless params[:user_id]
+
+    @author = User.find(params[:user_id])
+    @posts = @author.posts
   end
-  
+
   def show
     @post = Post.find(params[:user_id])
   end
