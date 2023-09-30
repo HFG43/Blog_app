feae564f9619af929fd799fe6fa410f39223d88a
HFG43 <hernanguemes@hotmail.com>
2023-09-29 21:03:21 -0300

    Misc: add form view in post new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,15 @@ class PostsController < ApplicationController
     @posts = @author.posts
   end
 
+  def new
+    respond_to do |format|
+      format.html { render :new }
+    end
+  end    
+
   def show
     @author = User.find(params[:user_id])
     @author_posts = @author.posts
-    @post = @author_posts.find(params[:id])
+    @post = @author_posts.find(params[:id])   
   end
 end
