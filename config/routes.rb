5da1501477b3664465c645911960a8e1e33148fb
HFG43 <hernanguemes@hotmail.com>
2023-09-30 11:13:22 -0300

    Feat: add comments route nested to post

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :new, :create, :show]
-  end
-
-  # get 'users/:user_id/posts/new', to: 'posts#new', as: 'new_user_post'
+    resources :posts, only: [:index, :new, :create, :show] do
+      resources :comments, only: [:new, :create]
+    end
+  end  
 
   root 'users#index'
 end
