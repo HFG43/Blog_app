b89d04484b1f64edfabf0fa20e561a5c6d0f2474
HFG43 <hernanguemes@hotmail.com>
2023-09-29 21:02:32 -0300

    Feat: add current_user method to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+
+	def current_user
+		@current_user
+	end
+
+	private
+
+	def set_current_user
+		@current_user = User.find(1)
+	end
 end
